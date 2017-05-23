9e61208bd3c0812ff60888f682e02bcc4a4c9aa4
ASUS <ASUS@DESKTOP-9JB2415>
2017-05-23 15:55:24 +0700

    coba lagu

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> nama = new List<string>();
+
+            nama.Add("kukuh");
+            nama.Add("erwan");
+            nama.Add("galih");
+
+            foreach (var a in nama)
+                Console.WriteLine(a);Console.ReadLine();
+
+            Console.WriteLine(nama.IndexOf("kukuh"));
+
+            nama.Insert(1, "wahyu");
+
+            foreach (var a in nama)
+                Console.WriteLine(a); Console.ReadLine();
+        }
+    }
+}
