5053b52dea12b8d7aa808cccf00cc327b8b99117
ASUS <ASUS@DESKTOP-9JB2415>
2017-05-23 11:40:28 +0700

    coba deh di update

---
+++ Fitness/Fitness/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int total = 0;
+
+            // looping
+            while (true)
+            {
+                // keluarkan pertanyaan
+                Console.WriteLine("Berapa lama Anda berolahraga");
+
+                // minta input dari user
+                input = Console.ReadLine();
+
+                // menambahkan inputnya
+                total += Int32.Parse(input);
+
+                // keluar bila user masukin "quit"
+                if (input == "quit")
+                {
+                    break;
+                }
+
+                // mengeluarkan inputnya
+                Console.WriteLine("Selamat, Anda sudah " + input + " menit berolahraga");
+
+            }
+            
+
+            
+
+
+        }
+    }
+}
