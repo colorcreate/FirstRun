a324f8d8b8438bd4388e5ce28ba4bd1fdc077f39
ASUS <ASUS@DESKTOP-9JB2415>
2017-05-24 14:55:21 +0700

    LINQ

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            IEnumerable<int> numbersBesarDariLima = new List<int>();
+
+            //foreach (var angka in numbers)
+            //{
+            //    if (angka > 5)
+            //    {
+            //        numbersBesarDariLima.Add(angka);
+            //    }
+            //}
+
+
+            //numbersBesarDariLima = from angka in numbers where angka > 5 select angka;  //Query syntax
+
+            numbersBesarDariLima = numbers.Where(n => n > 5); //method syntax
+
+
+            foreach (var number in numbersBesarDariLima)
+                Console.WriteLine(number);
+        }
+    }
+}
