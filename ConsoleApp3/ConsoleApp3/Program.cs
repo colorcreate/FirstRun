67ff0e43b887cbd7acd50c30b20f5d5dcd0d3a20
ASUS <ASUS@DESKTOP-9JB2415>
2017-05-24 11:10:00 +0700

    hashSet2

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -17,8 +17,8 @@ namespace ConsoleApp3
             nama.Add("galih");
             nama.Add("wahyu");
 
-            foreach (var a in nama)
-                Console.WriteLine(a);
+            //foreach (var a in nama)
+            //    Console.WriteLine(a);
 
 
             HashSet<int> evenNumber = new HashSet<int>();
@@ -30,8 +30,46 @@ namespace ConsoleApp3
             evenNumber.Add(10);
 
 
-            foreach (var number in evenNumber)
-                Console.WriteLine(number);
+            //foreach (var number in evenNumber)
+            //    Console.WriteLine(number);
+
+            HashSet<String> namaHash = new HashSet<String>();
+
+            namaHash.Add("kukuh");
+            namaHash.Add("galih");
+            namaHash.Add("daa");
+            namaHash.Add("daa");
+            namaHash.Add("kukuh");
+
+            //foreach (var name in namaHash)
+            //    Console.WriteLine(name);
+
+            //for (int i = 0; i < namaHash.Count(); i++)
+            //    Console.WriteLine(namaHash.ElementAt(i));
+
+            namaHash.Remove("daa"); //menghilangkan "daa" dari namaHash
+
+            ///
+            HashSet<String> namaHash2 = new HashSet<String>();
+            namaHash2.Add("ben");
+            namaHash2.Add("nila");
+            namaHash2.Add("YULIUS");
+            namaHash2.Add("HANI");
+
+            namaHash.UnionWith(namaHash2); //memasukkan semua unsur namaHash2 ke namaHash
+
+            for (int i=0; i<namaHash.Count(); i++)
+            {
+                if (namaHash.ElementAt(i)=="nila")
+                {
+                    Console.WriteLine("ada datanya");
+                }
+            }
+
+
+
+            Console.WriteLine("Panjang hashset: " + namaHash.Count());
+
         }
     }
 }
