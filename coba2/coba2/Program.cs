7cb32d253aa01189663f3be17b82d980097fe41b
ASUS <ASUS@DESKTOP-9JB2415>
2017-05-28 17:15:09 +0700

    ListOfListOfList

---
+++ coba2/coba2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfList = new List<List<List<int>>>();
+            listOfList.Add(new List<List<int>>());
+            listOfList.Add(new List<List<int>>());
+            listOfList.Add(new List<List<int>>());
+            listOfList.Add(new List<List<int>>());
+            listOfList.Add(new List<List<int>>());
+            listOfList.Add(new List<List<int>>());
+
+            listOfList[0].Add(new List<int>());
+            listOfList[0].Add(new List<int>());
+            listOfList[0].Add(new List<int>());
+
+            listOfList[0][0].Add(42);
+            listOfList[0][0].Add(41);
+            listOfList[0][1].Add(40);
+            listOfList[0][1].Add(39);
+            listOfList[0][2].Add(38);
+
+
+
+
+
+
+            Console.WriteLine(listOfList[0][1][1]);
+            Console.ReadLine();
+        }
+    }
+}
